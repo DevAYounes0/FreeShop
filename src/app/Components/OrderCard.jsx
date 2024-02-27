eb693951739a4ae55085e3e4aa4675cae6e951d9
sondossaad <sondosshalapy100@gmail.com>
2024-02-27 23:24:04 +0200

    sondos

---
+++ src/app/Components/OrderCard.jsx
@@ -0,0 +1,12 @@
+import { Card } from 'antd/lib';
+import React from 'react';
+
+const OrderCard = ({details}) => {
+  return (
+    <Card>
+      
+    </Card>
+  );
+}
+
+export default OrderCard;
